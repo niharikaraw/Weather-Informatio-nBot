3a8e06962d21b94c77b737527674fb53fcc8c077
niharikaraw <nrawat.kol@gmail.com>
2022-08-19 20:02:34 +0530

    added

--- bot/utils.py
+++ bot/utils.py
@@ -50,7 +50,7 @@ def reply_weather_info(data, uuid, city=None):
 
     I1.text(xy=(10, 220), text="{}".format(weather_cond), font=myFont, fill =(255, 0, 0))
     #I1.text(xy=(10, 180), text="{}".format(place), font=ImageFont.truetype("himalaya.ttf", 40), fill ='navy')
-    I1.text(xy=(10, 180), text="{}".format(place), font=ImageFont.truetype("Himalaya-Regular.ttf", 40), fill ='navy')
+    I1.text(xy=(10, 180), text="{}".format(place), font=ImageFont.truetype("Himalaya Regular.ttf", 40), fill ='navy')
     I1.text(xy=(250, 20), text="Temperature: {}°C".format(temp), font=myFont, fill =(255, 0, 0))
     I1.text(xy=(250, 50), text="Feels Like: {}°C".format(feels_like), font=myFont, fill =(255, 0, 0))
     I1.text(xy=(250, 80), text="Humidity: {} %".format(humidity), font=myFont, fill =(255, 0, 0))
