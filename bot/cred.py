daaa87b0eaf405a6e102cd9c54dddcf457e568f3
niharikaraw <nrawat.kol@gmail.com>
2022-12-06 01:04:09 +0530

    okay

--- bot/cred.py
+++ bot/cred.py
@@ -1,4 +1,4 @@
-bot_token = '5761837078:AAE8swqpRKGalpjwOaM4RKZXtDHIWn3-654'
-bot_user_name = 'OrderTrackingBobBot'
-#URL = 'https://weatherinformationbot.herokuapp.com/'
+bot_token = '5638078202:AAFWEkY1BWBssZiXm9RCWcqUYZiBN4CZV_M'
+bot_user_name = 'WeatherImformationBot'
+#URL = 'https://1dde-2405-201-4033-c066-1986-9383-5c11-5a6f.in.ngrok.io/'
 URL = 'https://niharikarawatprojects.tech/'
